fad1ff25afa6902b53a7deac439d483a8b73f72f
Pedro <pedrohazevedo@gmail.com>
2023-05-24 02:29:46 -0300

    debug=off

--- site_kim/settings.py
+++ site_kim/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ih(!tpx+vw_)*v*#4x==nx3kqhmqinczb6)(r4a%k0764x%zr0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
